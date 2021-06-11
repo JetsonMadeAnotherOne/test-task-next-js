dd7be9a9dbe3134652d9bb754e535bc6d65e7d62
Maxim Ustianov <maksym.ustianov@p-product.com>
2021-06-11 13:57:27 +0300

    refactor code

---
+++ pages/api/index.ts
@@ -0,0 +1,5 @@
+module.exports = async (req, res) => {
+    const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`);
+    const data = await response.json();
+    res.json(data);
+};
